630cd237c63e8cb42e5f0d9b301747de2a497f33
bao nguyen <kb.nguyen@free.fr>
2019-10-18 13:59:52 +0200

    ajout class hello world

---
+++ MyClass.java
@@ -0,0 +1,12 @@
+
+/**
+ * Hello-world.
+ *
+ * @author (Bao Nguyen)
+ * @version (18/10/2019)
+ */
+public class MyClass {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
